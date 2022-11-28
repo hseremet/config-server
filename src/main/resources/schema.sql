5dbd52766e7e5ca77760c1fd1395e6bca632f7ac
HAKAN SEREMET <hakan.seremet@turkcell.com.tr>
2022-11-28 09:32:41 +0300

    sql text changed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,18 @@
 DROP SCHEMA IF EXISTS CONFIG_SERVER CASCADE;
 
-CREATE SCHEMA IF NOT EXISTS CONFIG_SERVER;
+--CREATE SCHEMA IF NOT EXISTS CONFIG_SERVER;
 
-create table CONFIG_SERVER.PROPERTIES(
+create table PROPERTIES(
     ID BIGINT auto_increment PRIMARY KEY,
     APPLICATION varchar(8191),
     PROFILE varchar(8191),
     LABEL varchar(8191),
-    PROP_KEY varchar(8191),
-    PROP_VALUE varchar(32767),
+    "KEY" varchar(8191),
+    "VALUE" varchar(32767),
     primary key (ID)
 );
+
+INSERT INTO properties (ID, APPLICATION, PROFILE, LABEL, "KEY", "VALUE")
+VALUES (1,'config-server','dev','dev','configA','Config A');
+INSERT INTO properties (ID, APPLICATION, PROFILE, LABEL, "KEY", "VALUE")
+VALUES (2,'config-server','dev','dev','configB','Config B');
